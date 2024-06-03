73c4ba325063c96945abdca975c544b704e8e1c1
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-06-03 19:48:18 -0300

    Imagen en cards más grande. Arreglos de estilos en sección nosotros

--- frontend/src/pages/Nosotros.jsx
+++ frontend/src/pages/Nosotros.jsx
@@ -6,7 +6,7 @@ export const Nosotros = () => {
     return (
         <div>
             <Hero title={'Sobre nosotros'} urlImage={'https://cdn.pixabay.com/photo/2020/03/03/20/31/boat-4899802_1280.jpg'}></Hero>
-            <main className="main-nosotros flex-container">
+            <main className="main-nosotros">
                 <p>En el corazón de la majestuosa naturaleza de Mendoza, Argentina, nació un sueño hace más de 60 años. Todo comenzó con una charla entre amigos, en la que soñábamos con crear un refugio donde los viajeros pudieran encontrar paz y conexión con la naturaleza. Aquella charla se convirtió en nuestro primer proyecto: un pequeño alojamiento en las hermosas tierras de Mendoza, Argentina.</p>
                 <p>Desde entonces, hemos crecido y evolucionado, pero nuestro compromiso con la excelencia y la armonía con el entorno natural nunca ha cambiado. Cada alojamiento que construimos es más que un simple edificio; es un santuario donde nuestros huéspedes pueden escapar del bullicio de la vida cotidiana y reconectar consigo mismos y con el mundo que los rodea.</p>
                 <p>En NaturAlma, no solo ofrecemos estadías; creamos experiencias memorables. Desde la cálida bienvenida al momento de tu llegada hasta los detalles cuidadosamente diseñados en cada rincón de nuestras instalaciones, cada aspecto de tu estancia está pensado para superar tus expectativas y dejarte con recuerdos imborrables.</p>
