87fdc4171518c9dc08a74275f1db4e54e687750a
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-06-05 01:48:34 -0300

    Hero image en seccion admin, ahora el titulo tiene sombras

--- frontend/src/pages/TipoAlojamiento.jsx
+++ frontend/src/pages/TipoAlojamiento.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom"
 import { ListaTipoAlojamientos } from "../components/ListaTipoAlojamientos"
 import './tipoAlojamiento.css';
+import { Hero } from "../components/Hero";
 
 export const TipoAlojamiento = () => {
     return (
         <div className="inicio-section">
-            <div className="empty-div"></div>
-            <h2>Administración</h2>
-            <hr />
+            <Hero title={'Administración'} urlImage={'https://cdn.pixabay.com/photo/2015/01/08/18/25/desk-593327_960_720.jpg'}></Hero>
             <h3>Tipos de alojamientos</h3>
             <div className="flex-inline">
                 <Link to={`/tipoAlojamiento/agregar`} className="boton-primario"><i className="fa-solid fa-plus ff-icon"></i> Agregar nuevo</Link>
