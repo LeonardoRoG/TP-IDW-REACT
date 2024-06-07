f04d7bd92e081ef98458aea4551cea46ccd94f9d
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-06-07 20:10:06 -0300

    Seccion alojamientos y lista agregada

---
+++ frontend/src/pages/admin/Alojamiento.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { ListaAlojamientos } from '../../components/ListaAlojamientos'
+import { Link } from 'react-router-dom'
+
+export const Alojamiento = () => {
+
+    const BASE_URL = 'http://localhost:3001/';
+    const [data, setData] = useState([]);
+
+    const obtenerDatos = async () => {
+        try {
+            const response = await fetch(BASE_URL + 'alojamiento/getAlojamientos');
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error: ', error);
+        }
+    };
+
+    const eliminar = async (idAlojamiento) => {
+        try {
+            const response = await fetch(`${BASE_URL}alojamiento/deleteAlojamiento/${idAlojamiento}`,{
+                method: 'DELETE',
+                headers: { 'Content-Type':'application/json'},
+            });
+            if (response.ok) {
+                console.log('Eliminado');
+                obtenerDatos();
+            } else {
+                console.log('Error');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+      <>
+          <div className="flex-between">
+              <h2>Alojamientos</h2>
+              <Link to={`agregar`} className="boton-primario"><i className="fa-solid fa-plus ff-icon"></i>Agregar nuevo</Link>
+          </div>
+          <ListaAlojamientos data={data} obtenerDatos={obtenerDatos} eliminar={eliminar}></ListaAlojamientos>
+      </>
+    )
+}
