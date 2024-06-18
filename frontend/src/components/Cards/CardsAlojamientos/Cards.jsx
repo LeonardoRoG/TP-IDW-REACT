93fde17b65ca30029b28c60d4c0af7263bd7e867
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-06-17 21:24:00 -0300

    Pill de status  y fav agregados

--- frontend/src/components/Cards/CardsAlojamientos/Cards.jsx
+++ frontend/src/components/Cards/CardsAlojamientos/Cards.jsx
@@ -49,11 +49,66 @@ export const Cards = () => {
         };
         obtenerTipos();
     }, []);
+
+    const [dataImagenes, setDataImagenes] = useState([]);
+
+    useEffect(() => {
+        const obtenerImagenes = async () => {
+            try {
+                const response = await fetch(BASE_URL + 'imagen/getAllImagenes');
+                const jsonData = await response.json();
+                setDataImagenes(jsonData);
+                console.log(jsonData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        obtenerImagenes();
+    }, []);
+
+        /**
+     * @param {string} string recibe un string
+     * @returns string con la primera letra en mayúsculas
+     */
+        function capitalize(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        };
+
+        /**
+     * Función para generar colores claros aleatorios
+     * @returns código de color
+     */
+        const generateRandomColor = () => {
+            // Genera un color con luminosidad alta (entre 70% y 100%)
+            const luminosity = Math.floor(Math.random() * 31) + 70;
+            const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            const hexColor = `#${randomColor}`;
+            
+            // Convierte el color hexadecimal a RGB
+            const r = parseInt(hexColor.slice(1, 3), 16);
+            const g = parseInt(hexColor.slice(3, 5), 16);
+            const b = parseInt(hexColor.slice(5, 7), 16);
+            
+            // Calcula la luminosidad del color
+            const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+            
+            // Si la luminosidad es alta, devuelve el color; de lo contrario, genera otro
+            return luminance >= luminosity / 100 ? hexColor : generateRandomColor();
+        };
     
     if (dataCards.length > 0){
         return(dataCards.map(item =>
             <div key={item.idAlojamiento} className='card-item'>
-                <CardHeader data={item} dataTipos={dataTipos}></CardHeader>
+                <CardHeader data={item} dataTipos={dataTipos} dataImagenes={dataImagenes}></CardHeader>
+                <div className='card-medio'>
+                    <div className='type-pill' style={{backgroundColor:generateRandomColor()}}>
+                        <p>{dataTipos.map((tipo) => (
+                            item.idTipoAlojamiento === tipo.idTipoAlojamiento && capitalize(tipo.Descripcion)
+                        ))}
+                        </p>
+                    </div>
+                </div>
+
                 <div>
                     <div className='card-content'>
                         <h3>{item.Titulo}</h3>
