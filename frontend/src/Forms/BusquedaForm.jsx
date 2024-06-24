a6f54594198d22b6860306624857802961606674
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-06-24 00:18:51 -0300

    Filtro de busqueda completo y optimizado

--- frontend/src/Forms/BusquedaForm.jsx
+++ frontend/src/Forms/BusquedaForm.jsx
@@ -1,21 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
-
+import { getAllTiposAlojamientos } from "../services/tipoAlojamientoService";
 import './form.css';
 import { Button } from "../components/Button/Button";
 
 export const BusquedaForm = ({onSendData}) => {
 
     const [idTipoAlojamiento, setIdTipoAlojamiento] = useState(0);
+    const [precioMin, setPrecioMin] = useState(0);
     const [precioMax, setPrecioMax] = useState(0);
     const [estado, setEstado] = useState('');
+    const [cantidadDormitorios, setCantidadDormitorios] = useState(0);
+    const [cantidadBanios, setCantidadBanios] = useState(0);
     const form = useRef();
 
     const [dataTipos, setDataTipos] = useState([]);
 
     const obtenerTodos = async () => {
         try{
-            const response = await fetch('http://localhost:3001/tiposAlojamiento/getTiposAlojamiento');
-            const jsonData = await response.json();
+            const jsonData = await getAllTiposAlojamientos();
             setDataTipos(jsonData);
         } catch(err){
             console.error(err);
@@ -31,18 +33,42 @@ export const BusquedaForm = ({onSendData}) => {
 
         const json = {
             idTipoAlojamiento: idTipoAlojamiento,
+            precioMin: precioMin,
             precioMax: precioMax,
-            Estado: estado
+            Estado: estado,
+            CantidadDormitorios: cantidadDormitorios,
+            CantidadBanios: cantidadBanios
+        }
+        form.current.reset();
+        onSendData(json);
+    }
+
+    const limpiarBusqueda = (e) => {
+        e.preventDefault()
+        setIdTipoAlojamiento(0);
+        setPrecioMin(0);
+        setPrecioMax(0);
+        setEstado('');
+        setCantidadBanios(0);
+        setCantidadDormitorios(0);
+
+        const json = {
+            idTipoAlojamiento: 0,
+            precioMin: 0,
+            precioMax: 0,
+            Estado: "",
+            CantidadDormitorios: 0,
+            CantidadBanios: 0
         }
 
-        console.log(json);
         form.current.reset();
         onSendData(json);
     }
 
     return (
         <section className="section-flex">
-            <div className="flex-container-center">
+            <div className="flex-container-center buscador">
+                <h2>Filtros de búsqueda</h2>
                 <form ref={form} className="form-buscador">
                     <div className="form-field">
                         <label htmlFor="idTipoAlojamiento" className="form-label">Tipo de Alojamiento:</label>
@@ -54,8 +80,20 @@ export const BusquedaForm = ({onSendData}) => {
                         </select>
                     </div>
                     <div className="form-field">
-                        <label htmlFor="preciomax" className="form-label">Precio máximo:</label>
+                        <label htmlFor="precioMin" className="form-label">Precio mínimo(*):</label>
+                        <input type="number" id='precioMin' min={0} name="precioMin" value={precioMin} onChange={e => setPrecioMin(e.target.value)} className="form-input" placeholder=""/>
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="preciomax" className="form-label">Precio máximo(*):</label>
                         <input type="number" id='preciomax' min={0} name="preciomax" value={precioMax} onChange={e => setPrecioMax(e.target.value)} className="form-input" placeholder=""/>
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="cantidadDormitorios" className="form-label">Cantidad de dormitorios(*):</label>
+                        <input type="number" id='cantidadDormitorios' min={0} name="cantidadDormitorios" value={cantidadDormitorios} onChange={e => setCantidadDormitorios(e.target.value)} className="form-input" placeholder=""/>
+                    </div>
+                    <div className="form-field">
+                        <label htmlFor="cantidadBanios" className="form-label">Cantidad de baños(*):</label>
+                        <input type="number" id='cantidadBanios' min={0} name="cantidadBanios" value={cantidadBanios} onChange={e => setCantidadBanios(e.target.value)} className="form-input" placeholder=""/>
                     </div>
                     <div className="form-field">
                         <label htmlFor="estado" className="form-label">Estado:</label>
@@ -65,10 +103,12 @@ export const BusquedaForm = ({onSendData}) => {
                             <option value="Reservado">RESERVADO</option>
                         </select>
                     </div>
-                    <div className="form-field">
-                        <Button onClick={sendChildData} color='primary' rounded shadowed grow icon='search'>Buscar</Button>
+                    <div className="columna-botones-buscador">
+                        <Button onClick={sendChildData} color='primary' rounded shadowed icon='search'>Buscar</Button>
+                        <Button onClick={limpiarBusqueda} color='danger' rounded shadowed icon='cancel'>Limpiar</Button>
                     </div>
                 </form>
+                <span>* Si es 0 se muestran todos los resultados</span>
             </div>
         </section>
     )
--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,21 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { BusquedaForm } from '../Forms/BusquedaForm';
 import { Button } from '../components/Button/Button';
 import { CardLugar } from '../components/Cards/CardLugar';
 import { Cards } from '../components/Cards/CardsAlojamientos/Cards';
 import { Hero } from '../components/Hero';
+import { getAllTiposAlojamientos } from '../services/tipoAlojamientoService';
 import './inicio.css';
+import { getAllAlojamientos } from '../services/alojamientoService';
+import { getAllImagenes } from '../services/imagenService';
+import { getAllServicios } from '../services/servicioService';
+import { getAllAlojamientoServicios } from '../services/alojamientoServicioService';
 
 export const Inicio = () =>{
 
-    const BASE_URL = 'http://localhost:3001/';
     const [dataCards, setDataCards] = useState([]);
 
     useEffect(() => {
         const obtenerDatos = async () => {
             try {
-                const response = await fetch(BASE_URL + 'alojamiento/getAlojamientos');
-                const jsonData = await response.json();
+                const jsonData = await getAllAlojamientos();
                 setDataCards(jsonData);
             } catch (error) {
                 console.error('Error: ', error);
@@ -29,8 +32,7 @@ export const Inicio = () =>{
     useEffect(() => {
         const obtenerTipos = async () => {
             try{
-                const response = await fetch('http://localhost:3001/tiposAlojamiento/getTiposAlojamiento');
-                const jsonData = await response.json();
+                const jsonData = await getAllTiposAlojamientos();
                 setDataTipos(jsonData);
             } catch(err){
                 console.error(err);
@@ -44,8 +46,7 @@ export const Inicio = () =>{
     useEffect(() => {
         const obtenerImagenes = async () => {
             try {
-                const response = await fetch(BASE_URL + 'imagen/getAllImagenes');
-                const jsonData = await response.json();
+                const jsonData = await getAllImagenes();
                 setDataImagenes(jsonData);
             } catch (error) {
                 console.error(error);
@@ -59,8 +60,7 @@ export const Inicio = () =>{
     useEffect(() => {
         const obtenerServicios = async () => {
             try {
-                const response = await fetch(BASE_URL + 'servicio/getAllServicios');
-                const jsonData = await response.json();
+                const jsonData = await getAllServicios();
                 setDataServicios(jsonData);
             } catch (err) {
                 console.error(err);
@@ -74,8 +74,7 @@ export const Inicio = () =>{
     useEffect(() => {
         const obtenerServiciosAsociados = async () => {
             try {
-                const response = await fetch(BASE_URL + 'alojamientosServicios/getAllAlojamientoServicios');
-                const jsonData = await response.json();
+                const jsonData = await getAllAlojamientoServicios();
                 setDataAsociados(jsonData);
             } catch (err){
                 console.error(err);
@@ -84,78 +83,122 @@ export const Inicio = () =>{
         obtenerServiciosAsociados();
     }, []);
 
-    const [mostrarData, setMostrarData] = useState([]);
     const [childData, setChildData] = useState({
         idTipoAlojamiento: 0,
+        precioMin: 0,
         precioMax: 0,
-        Estado: 'Todos'
+        Estado: 'Todos',
+        CantidadDormitorios: 0,
+        CantidadBanios: 0
     });
 
     const handleChildData = (dataFromChild) => {
         setChildData(dataFromChild);
-    }
+    };
 
-    useEffect(() => {
-        const filtrarDatos = (item) => {
+    const filtrarPorPrecioMin = (item) => {
+        if (Number(childData.precioMin) !== 0){
             return (
-                (Number(item.PrecioPorDia) < Number(childData.precioMax)) &&
-                (Number(item.idTipoAlojamiento) === Number(childData.idTipoAlojamiento))
-            );
-        };
+                (Number(item.PrecioPorDia) > Number(childData.precioMin))
+            )
+        } else {
+            return item;
+        }
+    };
 
-        const filtrarTodosPorPrecio = (item) => {
+    const filtrarPorPrecioMax = (item) => {
+        if (Number(childData.precioMax) !== 0){
             return (
                 (Number(item.PrecioPorDia) < Number(childData.precioMax))
             )
+        } else {
+            return item;
         }
+    };
 
-        const filtrarPorTipo = (item) => {
+    const filtrarPorTipo = (item) => {
+        if (Number(childData.idTipoAlojamiento) !== 0){
             return (
                 (Number(item.idTipoAlojamiento) === Number(childData.idTipoAlojamiento))
             )
-        }
-    
-        if (Number(childData.precioMax) === 0 && Number(childData.idTipoAlojamiento) === 0) {
-            setMostrarData(dataCards);
-        } else if (Number(childData.precioMax) !== 0 && Number(childData.idTipoAlojamiento) === 0){
-            setMostrarData(dataCards.filter(filtrarTodosPorPrecio));
-        } else if (Number(childData.precioMax) === 0 && Number(childData.idTipoAlojamiento) !== 0){
-            setMostrarData(dataCards.filter(filtrarPorTipo));
         } else {
-            setMostrarData(dataCards.filter(filtrarDatos));
+            return item;
         }
-    }, [childData, dataCards]);
+    };
+
+    const filtrarPorEstado = (item) => {
+        if (String(childData.Estado) === 'Disponible' || String(childData.Estado) === 'Reservado'){
+            return (
+                (String(item.Estado) === String(childData.Estado))
+            )
+        } else {
+            return item;
+        }
+    };
+
+    const filtrarPorDormitorios = (item) => {
+        if (Number(childData.CantidadDormitorios) !== 0){
+            return (
+                (Number(item.CantidadDormitorios) === Number(childData.CantidadDormitorios))
+            )
+        } else {
+            return item;
+        }
+    };
+
+    const filtrarPorBanios = (item) => {
+        if (Number(childData.CantidadBanios) !== 0){
+            return (
+                (Number(item.CantidadBanios) === Number(childData.CantidadBanios))
+            )
+        } else {
+            return item;
+        }
+    };
+
+    const sectionRef = useRef();
+    const scrollAction = () => {
+        if (sectionRef.current) {
+            const yOffset = sectionRef.current.getBoundingClientRect().top + window.scrollY;
+            window.scrollTo({top: yOffset -50, behavior: 'smooth'})
+        }
+    }
 
     return (
         <div className='inicio-section'>
-            <Hero urlImage={'./img/lago-home.jpg'} position={'center'}>
+            <Hero urlImage={'./img/lago-home.jpg'} position={'center'} heigth={'80vh'}>
                 <div className='hero-inicio'>
                     <h1>Viajá por toda la Argentina.</h1>
                     <p>Las mejores experiencias te esperan.</p>
-                    <div>
-                        <Button icon='send' extrarounded shadowed>Comenzar</Button>
+                    <div className='boton-comenzar'>
+                        <Button onClick={scrollAction} icon='send' extrarounded shadowed>Comenzar</Button>
                     </div>
                 </div>
             </Hero>
-            <h2>Elegí tu destino: </h2>
-            {childData? childData.idTipoAlojamiento : null}
-            <BusquedaForm onSendData={handleChildData}></BusquedaForm>
-            <h2>Alojamientos en oferta</h2>
-            <section className='flex-container'>
-                <Cards
-                    dataCards={mostrarData}
-                    dataTipos={dataTipos}
-                    dataServicios={dataServicios}
-                    dataImagenes={dataImagenes}
-                    dataAsociados={dataAsociados}
-                ></Cards>
-            </section>
-            <h2>Destinos destacados</h2>
-            <section className='flex-container'>
-                <CardLugar title='Cataratas' subtitle='desde $79.000,-' urlImagen='https://cdn.pixabay.com/photo/2014/11/01/01/57/mouth-of-the-iguassu-511500_960_720.jpg'></CardLugar>
-                <CardLugar title='Bariloche' subtitle='desde $139.000,-' urlImagen='https://cdn.pixabay.com/photo/2019/02/25/20/23/nature-4020525_960_720.jpg'></CardLugar>
-                <CardLugar title='Mar del Plata' subtitle='desde $160.000,-' urlImagen='https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_1280.jpg'></CardLugar>
-            </section>
+            <main className='main-inicio'>
+                <BusquedaForm onSendData={handleChildData}></BusquedaForm>
+                <h2 ref={sectionRef}>Alojamientos en oferta</h2>
+                <section className='flex-container' id='datos'>
+                    <Cards
+                        dataCards={dataCards.filter(filtrarPorPrecioMin)
+                            .filter(filtrarPorPrecioMax)
+                            .filter(filtrarPorTipo)
+                            .filter(filtrarPorEstado)
+                            .filter(filtrarPorBanios)
+                            .filter(filtrarPorDormitorios)}
+                        dataTipos={dataTipos}
+                        dataServicios={dataServicios}
+                        dataImagenes={dataImagenes}
+                        dataAsociados={dataAsociados}
+                    ></Cards>
+                </section>
+                <h2>Destinos destacados</h2>
+                <section className='flex-container'>
+                    <CardLugar title='Cataratas' subtitle='desde $79.000,-' urlImagen='https://cdn.pixabay.com/photo/2014/11/01/01/57/mouth-of-the-iguassu-511500_960_720.jpg'></CardLugar>
+                    <CardLugar title='Bariloche' subtitle='desde $139.000,-' urlImagen='https://cdn.pixabay.com/photo/2019/02/25/20/23/nature-4020525_960_720.jpg'></CardLugar>
+                    <CardLugar title='Mar del Plata' subtitle='desde $160.000,-' urlImagen='https://cdn.pixabay.com/photo/2022/06/09/10/13/beach-7252178_1280.jpg'></CardLugar>
+                </section>
+            </main>
         </div>
     )
 }
