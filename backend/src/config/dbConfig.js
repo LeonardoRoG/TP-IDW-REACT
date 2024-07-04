7665a0e57e87c11386306bab4265176960cb02e5
Alejo Leonardo Rosas <109430362+LeonardoRoG@users.noreply.github.com>
2024-07-04 16:25:32 -0300

    sql connection string

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Password123-',
-  database: 'idw',
+  host: 'sql10.freemysqlhosting.net',
+  user: 'sql10717620',
+  password: 'SewbRZrieK',
+  database: 'sql10717620',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
